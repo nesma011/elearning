a4705ff7a0816da08bceac44826f0b8cf1750b72
nesma011 <nesmaatef349@gmail.com>
2025-03-03 21:33:35 +0200

    hi

--- src/Components/Analytics/RadarScore.jsx
+++ src/Components/Analytics/RadarScore.jsx
@@ -86,11 +86,11 @@ const RadarScore = () => {
 
       <div className="flex items-center justify-center p-4">
         <RadarChart
-          cx={200}
-          cy={200}
+          cx={250}
+          cy={250}
           outerRadius={100}
           width={600}
-          height={500}
+          height={600}
           data={analytics}
         >
           <PolarGrid stroke="#e5e5e5" />
