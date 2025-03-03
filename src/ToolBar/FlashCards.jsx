2828bb0157efb3bdef434b53cb02ddb1e51c70d7
nesma011 <nesmaatef349@gmail.com>
2025-03-03 20:37:49 +0200

    ok

--- src/ToolBar/FlashCards.jsx
+++ src/ToolBar/FlashCards.jsx
@@ -55,7 +55,7 @@ const Flashcards = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQwODQ2NjQ4LCJpYXQiOjE3NDAyNDE4NDYsImp0aSI6IjU0ZTVkNWJlN2Q3ZDRkMjk4OTYzNjhmYmJmNTlkMjkxIiwidXNlcl9pZCI6NjZ9.sZRJuReyOg4ZaIK-Z4cMhcgS2svPKOLbaAcF4I1oSF4`
+          "Authorization": `Bearer ${token}`
         },
         body: JSON.stringify(formData)
       });
