365cfe2aa8a46740f15e1b52c8804a677ef19489
nesma011 <nesmaatef349@gmail.com>
2025-03-08 16:36:06 +0200

    ok

--- src/Components/CreateTest/CreateTest.jsx
+++ src/Components/CreateTest/CreateTest.jsx
@@ -525,11 +525,7 @@ const ErrorBoundary = ({children}) => {
   ? 'count_question_unanswer'
   : 'count_question';
 
-  const subjectCountKey = showIncorrect
-  ? 'count_question_field'
-  : showUnanswered
-  ? 'count_question_unanswer'
-  : 'count_question';
+
 
 const subtitleCountKey = showIncorrect
   ? 'field'
@@ -537,12 +533,7 @@ const subtitleCountKey = showIncorrect
   ? 'unanswer'
   : 'subtitles_remaining';
 
-/*   const allCountKey = showIncorrect
-  ? 'field'
-  : showUnanswered
-  ? 'unanswer'
-  : 'subtitles_remaining'; */
-  
+
 
 
   if (!yearId) {
@@ -690,7 +681,7 @@ const subtitleCountKey = showIncorrect
                             className="w-4 h-4"
                           />
                           <span>{subject.name}</span>
-                          <span className="text-green-600 mx-2">({subject[subjectCountKey] || 0})</span>
+                          <span className="text-green-600 mx-2">({subject.count_question || 0})</span>
                           
                         </div>
                       ))}
