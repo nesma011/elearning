ca2a13541e7c872efaa749c5c2178a22c3dbf288
nesma011 <nesmaatef349@gmail.com>
2025-03-10 01:00:39 +0200

    ok

--- src/Components/Test/Test.jsx
+++ src/Components/Test/Test.jsx
@@ -611,7 +611,7 @@ export default function Test() {
             Item {currentQuestionIndex + 1} of {testData.questions?.length}
           </span>
           <h2 className="text-xl pt-1 font-bold">
-            Question ID: {testData?.test_id || 'N/A'}
+            Question ID: {testData?.id_question || 'N/A'}
           </h2>
         </div>
 
