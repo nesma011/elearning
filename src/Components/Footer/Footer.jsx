e09bd33b8e8bbcdbe92335f52df2e448e1cb6d3f
nesma011 <nesmaatef349@gmail.com>
2025-03-10 09:09:41 +0200

    kk

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from "../../assets/logo.webp"
+import logo from "../../../public/logo.webp"
 import { NavLink } from 'react-router-dom'
 
 export default function Footer() {
