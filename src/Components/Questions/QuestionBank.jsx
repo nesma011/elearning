9dc16e95f80efe468d46a962a2cbb7f827dcb043
nesma011 <nesmaatef349@gmail.com>
2025-03-15 03:02:11 +0200

    font

--- src/Components/Questions/QuestionBank.jsx
+++ src/Components/Questions/QuestionBank.jsx
@@ -84,13 +84,13 @@ export default function QuestionBank() {
                   </h2>
                   <div className="flex justify-center gap-4 pt-8">
                     <NavLink to="https://www.facebook.com/share/18aNvC8sCJ/">
-                      <img src={face} alt="Facebook" className="w-10" />
+                      <img src={face} alt="Facebook" className="w-14" />
                     </NavLink>
                     <NavLink to="https://wa.me/201229733297">
-                      <img src={whats} alt="WhatsApp" className="w-10" />
+                      <img src={whats} alt="WhatsApp" className="w-14" />
                     </NavLink>
                     <NavLink to="https://t.me/ddfffgry">
-                      <img src={telegram} alt="Telegram" className="w-10" />
+                      <img src={telegram} alt="Telegram" className="w-14" />
                     </NavLink>
                   </div>
                 </div>
